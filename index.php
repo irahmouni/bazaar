3c82e2f40bba8cbb0dd9bec8ea66c9b9a3cf9378
karim RAHMOUNI <karim.rahmouni@live.fr>
2020-07-13 13:00:12 +0200

    apparétre des élément sur l'affichage

--- index.php
+++ index.php
@@ -14,18 +14,133 @@ try {
 /**
  * la partie exécutable
  */
-$sql = 'SELECT * FROM item';
+/**
+ * partie adresse
+ */
+$sql = 'SELECT * FROM Adresse';
 try {
     $stmt = $db->prepare($sql);
     $stmt->execute();
   
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
-    print $row[ 'nom' ];
-    print $row[ 'descr' ];
+    print $row[ 'adresseID' ];
+    print $row[ 'numero' ];
+    print $row[ 'rue' ];
+    print $row[ 'codePostale' ];
+    print $row[ 'pays' ];
     //print $row[ 'url' ];
     }
 }catch (PDOException $e){
         print $e->getMessage(); 
 }
-    
+
+/**
+ * partie Article
+ */
+$sql = 'SELECT * FROM Article';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'ArticleID' ];
+    print $row[ 'nom' ];
+    print $row[ 'descriptionCourte' ];
+    print $row[ 'prix HT' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+
+/**
+ * partie commande
+ */
+$sql = 'SELECT * FROM Commande';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'commandeID' ];
+    print $row[ 'reglee' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+   
+/**
+ * partie IllustrationArticle
+ */
+$sql = 'SELECT * FROM IllustrationArticle';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'ImageID' ];
+    print $row[ 'articleID' ];
+    print $row[ 'title' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+/**
+ * partie IllustrationArticle
+ */
+$sql = 'SELECT * FROM Operation';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'operationID' ];
+    print $row[ 'quantite' ];
+    print $row[ 'date' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+   
+/**
+ * partie Prix
+ */
+$sql = 'SELECT * FROM Prix';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'articleID' ];
+    print $row[ 'valeur' ];
+    print $row[ 'article' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+   
+/**
+ * partie Utilisateur
+ */
+$sql = 'SELECT * FROM Utilisateur';
+try {
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC) ){
+    print $row[ 'id' ];
+    print $row[ 'passwordHash' ];
+    print $row[ 'pseudo' ];
+    print $row[ 'prenom' ];
+    print $row[ 'nom' ];
+    //print $row[ 'url' ];
+    }
+}catch (PDOException $e){
+        print $e->getMessage(); 
+}
+   
 ?>
