c3dfb90fb0235af53eb28af90e085dace1c14243
karim RAHMOUNI <karim.rahmouni@live.fr>
2020-07-15 22:26:31 +0200

    html

--- indexOrg.php
+++ indexOrg.php
@@ -136,123 +136,109 @@ try {
    
 ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Welcome to ebuybd online shop</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		<script src="/js/homeslideshow.js"></script>
-	</head>
-	<body style="min-width: 980px;">
-		<div class="homepageheader" style="position: relative;">
-			<div class="signinButton loginButton">
-				<div class="uiloginbutton signinButton loginButton" style="margin-right: 40px;">
-					<?php 
-						if ($user!="") {
-							echo '<a style="text-decoration: none; color: #fff;" href="logout.php">LOG OUT</a>';
-						}
-						else {
-							echo '<a style="color: #fff; text-decoration: none;" href="signin.php">SIGN IN</a>';
-						}
-					 ?>
-					
-				</div>
-				<div class="uiloginbutton signinButton loginButton" style="">
-					<?php 
-						if ($user!="") {
-							echo '<a style="text-decoration: none; color: #fff;" href="profile.php?uid='.$user.'">Hi '.$uname_db.'</a>';
-						}
-						else {
-							echo '<a style="text-decoration: none; color: #fff;" href="login.php">LOG IN</a>';
-						}
-					 ?>
-				</div>
-			</div>
-			<div style="float: left; margin: 5px 0px 0px 23px;">
-				<a href="index.php">
-					<img style=" height: 75px; width: 130px;" src="image/ebuybdlogo.png">
-				</a>
-			</div>
-			<div class="">
-				<div id="srcheader">
-					<form id="newsearch" method="get" action="search.php">
-					        <input type="text" class="srctextinput" name="keywords" size="21" maxlength="120"  placeholder="Search Here..."><input type="submit" value="search" class="srcbutton" >
-					</form>
-				<div class="srcclear"></div>
-				</div>
-			</div>
-		</div>
-		<div class="home-welcome">
-			<div class="home-welcome-text" style="background-image: url(image/homebackgrndimg.png); height: 380px; ">
-				<h1 style="margin: 0px;">Welcome To eBuyBD</h1>
-				<h2>bazaar</h2>
-			</div>
-		</div>
-		<div class="home-prodlist">
-			<div>
-				<h3 style="text-align: center;">Products Category</h3>
-			</div>
-			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/saree.php">
-							<img src="./image/product/saree/new-designer-fancy-look-attractive-saree-2-original.jpg" class="home-prodlist-imgi">
-							</a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/perfume.php">
-							<img src="./image/product/perfume/Most-Popular-Perfumes-for-Women10.png" class="home-prodlist-imgi">
-							</a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/hijab.php">
-							<img src="./image/product/saree/hijab 1.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/toilatry.php">
-							<img src="./image/product/beauty/toiletries.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/footwear.php">
-							<img src="./image/product/footwear/footwear1.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/tshirt.php">
-							<img src="./image/product/saree/tshirts1.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/watch.php">
-							<img src="./image/product/watch/watches1.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-				<ul style="float: left;">
-					<li style="float: left; padding: 25px;">
-						<div class="home-prodlist-img"><a href="women/ornament.php">
-							<img src="./image/product/ornament/earrings1.png" class="home-prodlist-imgi"></a>
-						</div>
-					</li>
-				</ul>
-			</div>			
-		</div>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bazaar</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<br>
+<div class="container-fluid bandeau">
+    <div class="row">
+        <h1 class="col-8">bazaar</h1>
+        <div class="col-2">
+            <p id="article">panier vide</p>
+        </div>
+        <div class="col-2">
+            <img  id=panier src="img/panier.jpg">
+        </div>
+
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img1" src="img/adidas_predator.jpg">
+            <h5>Adidas Predator Mania</h5>
+            <button id="0" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>155.25€ TTC</p>
+        </article>
+
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img2" src="img/adidas_pure.jpg">
+            <h5>Adidas Ace PureControl</h5>
+            <button id="1" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>175.25€ TTC</p>
+        </article>
+
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img3" src="img/adidas_purechaos.jpg">
+            <h5>Adidas X16 Purechaos</h5>
+            <button id="2" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>125.25€ TTC</p>
+        </article>
+
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img4" src="img/nike_hyperv.jpg">
+            <h5>Nike Hypervenom Phantom III</h5>
+            <button id="3" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>145.25€ TTC</p>
+        </article>
+
+
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img5" src="img/nike_tiempo.jpg">
+            <h5>Nike Tiempo Ligeria</h5>
+            <button id="4" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>165.25€ TTC</p>
+        </article>
+
+        <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img id="img6" src="img/nike_mercu.jpg">
+            <h5>Nike Mercurial Superfly</h5>
+            <button id="5" class="btn btn-success" onclick="achat(this), total()">Ajouter au panier</button>
+            <p>105.25€ TTC</p>
+        </article>
+
+    </div>
+
+</br>
+</br>
+
+    <div class="row">
+        <div class="col-12">
+            <h5 id="tot" >Total du panier : 0€ </h5>
+        </div>
+        </br>
+        <div class="col-12">
+            <h8 id="tva" ></h8>
+        </div>
+    </div>
+    </br>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Produit</th>
+            <th>Quantité</th>
+            <th>P.U</th>
+            <th>Total</th>
+        </tr>
+
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+
+
+
+
+
+</div>
+<script src="jquery.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</body>
 </html>
